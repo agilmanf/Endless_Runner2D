4b5af3ff9c5ae92b5fcafc8ca4d9c0b2f6465f64
Gilman Firdaus <agilmanf@gmail.com>
2021-10-24 21:14:44 +0700

    Adding Background & Script it

---
+++ Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    public Transform camera;
+    public GameObject bg;
+
+    private Vector3 generatePos;
+
+    private void Start()
+    {
+        generatePos = new Vector3(19,0,1);
+        GenerateBackground();
+    }
+
+    void LateUpdate()
+    {
+        ///Debug.Log(generatePos.position);
+        this.transform.position = camera.position;
+    }
+
+    public void GenerateBackground()
+    {
+        Instantiate(bg, generatePos,this.transform.rotation,this.transform);
+    }
+}
---
+++ Assets/Scripts/BackgroundMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMovement : MonoBehaviour
+{
+    private Transform parent;
+    private float destroyPos = 19;
+    private Vector3 generatePos;
+
+    private void Start()
+    {
+        parent = transform.parent.GetComponent<Transform>();
+    }
+
+    private void LateUpdate()
+    {
+        generatePos = new Vector3(parent.position.x + destroyPos, 0, 1);
+        transform.Translate(-1 * Time.deltaTime,0,0);
+
+        // Recycle the background
+        if (this.transform.position.x + destroyPos < parent.position.x)
+        {
+            this.transform.position = generatePos;
+        }
+    }
+}
